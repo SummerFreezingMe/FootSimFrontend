9a5a954432866ad82748569241f41f203003da8e
79969 <m.byckoff2015@yandex.ru>
2023-08-11 20:50:22 +0300

    feat: add app navbar

--- footsim_frontend/src/App.js
+++ footsim_frontend/src/App.js
@@ -1,40 +1,23 @@
-import {Component} from "react";
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Players from "./pages/players";
+import PlayerEdit from "./pages/playerEdit";
+
 
 class App extends Component {
-  state = {
-    players: []
-  };
-  logo;
-
-  async componentDidMount() {
-    const response = await fetch('/players/get_all', {
-      method: 'GET',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      }
-    });
-    const body = await response.json();
-    this.setState({players: body});
-  }
-
   render() {
-    const {players} = this.state;
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={this.logo} className="App-logo" alt="logo" />
-            <div className="App-intro">
-              <h2>Clients</h2>
-              {players.map(player =>
-                  <div key={player.id}>
-                    {player.name} ({player.rating})
-                  </div>
-              )}
-            </div>
-          </header>
-        </div>
-    );
+        <Router>
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/players' exact={true} element={<Players />}/>
+            <Route path='/players/:id' element={<PlayerEdit />}/>
+          </Routes>
+        </Router>
+    )
   }
 }
+
 export default App;
